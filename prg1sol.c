a8e41a4bcd446411d5f365fcca3cc8fce5d527f1
Nandu <149227694+nandinivc37@users.noreply.github.com>
2025-02-17 12:26:13 +0530

    Update prg1sol.c

--- prg1sol.c
+++ prg1sol.c
@@ -2,6 +2,51 @@
 #include<stdio.h>
 #include<string.h>
 
+void employeeInput(char* p_name, int* p_id, int* p_days_worked, float* p_salary){
+    printf("Enter Employee name : \n");
+    scanf("%s", p_name);
+
+    printf("Enter employee ID : \n");
+    scanf("%d", p_id);
+
+    printf("Enter no. of days worked in month(max 30) : \n");
+    scanf("%d", p_days_worked);
+
+    printf("Enter monthly salary : \n");
+    scanf("%f", p_salary);
+}
+
+float calculateBonus(int days_worked, float salary, char* category){
+    float bonus = 0;
+    
+    if(days_worked > 25){
+        bonus = salary * 0.05;
+        salary = salary + bonus;
+        strcpy(category, "excellent");
+    } else if(days_worked < 10){
+        salary = salary / 2;
+    }
+
+    if(days_worked < 15){
+        strcpy(category, "Needs improvement");
+    }
+
+    return bonus;
+}
+
+void printEmployeeDetails(char* name, int id, int days_worked, float salary, float bonus, char* category){
+    printf("\n Employee details after bonus\n");
+    printf("Employee name: %s\n", name);
+    printf("Employee ID: %d\n", id);
+    printf("NO. of days worked in month: %d\n", days_worked);
+    printf("Employee Final Salary : %.2f\n", salary);
+    printf("Performance: %s\n", category);
+    
+    if(bonus > 0){
+        printf("Bonus Applied: %.2f \n", bonus);
+    }
+}
+
 int main(){
     char name[30];
     int days_worked;
@@ -10,46 +55,15 @@ int main(){
     char category[20] = "good";
     float bonus = 0;
 
-    //taking user input
-    printf("Enter Employee name : \n");//name
-    scanf("%s", name);
+    // Taking user input
+    employeeInput(name, &id, &days_worked, &salary);
 
-    printf("Enter employee ID : \n");//ID
-    scanf("%d", &id);
-
-    printf("Enter no. of days worked in month(max 30) : \n");
-    scanf("%d", &days_worked);
-
-    printf("Enter monthly salary : \n");
-    scanf("%f", &salary);
+    // Calculating bonus
+    bonus = calculateBonus(days_worked, salary, category);
     
-    
-    //calculating bonus if applicable
-    if(days_worked > 25){
-        float bonus = salary*0.05;
-        salary = salary+bonus;
-        strcpy(category, "exellent");
-
-    } else if(days_worked < 10){
-        salary = salary/2;
-    }
-    
-    if(days_worked<15){
-        strcpy(category,"Need improvement");
-    }
-
-    //displaying details
-    printf("\n Employee details after bonus\n");
-    printf("Employee name: %s\n", name);
-    printf("Employee ID: %d\n", id);
-    printf("NO. of days worked in month: %d\n", days_worked);
-    printf("Employee Final Salary : %0.2f\n", salary);
-    
-    printf("Performance : %s", category);
-    if(bonus>0){
-        printf("Bonus Applied : %d \n",bonus);
-    }
+    // Displaying details
+    printEmployeeDetails(name, id, days_worked, salary, bonus, category);
 
     return 0;
-
 }
+
